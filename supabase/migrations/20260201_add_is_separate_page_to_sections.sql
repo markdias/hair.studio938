d66b0c3c3279860eb1d7a76eaa9e4a10f33575b7
Mark Dias <mark@mdias.co.uk>
2026-02-04 16:48:53 +0000

    Docs: Update Deployment Guide and consolidate DB schema

--- supabase/migrations/20260201_add_is_separate_page_to_sections.sql
+++
@@ -1,5 +0,0 @@
--- Add is_separate_page column to site_page_sections
-ALTER TABLE site_page_sections ADD COLUMN IF NOT EXISTS is_separate_page BOOLEAN DEFAULT false;
-
--- Add is_separate_page column to custom_sections
-ALTER TABLE custom_sections ADD COLUMN IF NOT EXISTS is_separate_page BOOLEAN DEFAULT false;
